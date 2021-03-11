8c8256e250e5ac3ea8cad5ffeb93cb4277bc84e0
MSI\shkar <shkarshahab1@gmail.com>
2021-03-11 16:34:18 +0300

    change products h1 title

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -30,7 +30,7 @@
                 <div class="col-md-12">
                     <div class="card card-primary">
                         <div class="card-header">
-                            <h3 class="card-title">Add Market</h3>
+                            <h3 class="card-title">Add Product</h3>
                         </div>
                         <!-- /.card-header -->
                         <!-- form start -->
